fc183c59e9af3bbdb730bfe3546d727f8d108cc2
JaydipMakwana <jaydip.makwana@botreetechnologies.com>
2019-12-16 13:29:09 +0530

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..250).to_a.each do |number|
+  Article.find_or_create_by(title: "Article-#{number}", content: "This is article number #{number}")
+end
